ada493ad0c781f4789dbc712a8b26485f3e0de8e
Alex Sandro <alex.sandro.1199@gmail.com>
2023-12-20 16:23:50 -0300

    Bloco

--- src/main/java/com/alexsanjr/modelodominioorm/entities/Atividade.java
+++ src/main/java/com/alexsanjr/modelodominioorm/entities/Atividade.java
@@ -2,7 +2,9 @@ package com.alexsanjr.modelodominioorm.entities;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Atividade {
@@ -19,6 +21,9 @@ public class Atividade {
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade")
+    private Set<Bloco> blocos = new HashSet<>();
 
     public Atividade() {
     }
@@ -68,6 +73,10 @@ public class Atividade {
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+    }
+
+    public Set<Bloco> getBlocos() {
+        return blocos;
     }
 
     @Override
